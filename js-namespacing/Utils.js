f2412dde4e1b21428ae8ae3532abf1456f90a581
Brian Salvati <salvatied@gmail.com>
2011-12-23 23:52:14 -0500

    made initialization immediate

--- js-namespacing/Utils.js
+++ js-namespacing/Utils.js
@@ -1,10 +1,10 @@
 BLOG.namespace('BLOG.utils');
-BLOG.utils = function(){
+BLOG.utils = (function(){
 
     var tenSecondDelay = $.proxy(setTimeout.partial(undefined, 10000),this);
 
     return{
         delayTenSeconds:tenSecondDelay
     }
-}();
+}());
 
